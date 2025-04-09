1d92c85b1bbd9c1accec825f367384e980ac3ea8
Emmanuel-Diko <emmanuelnnachi@punch.agency>
2025-04-09 14:30:59 -0700

    first commit

--- app.js
+++ app.js
@@ -6,13 +6,19 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    
+    
+    // Add your deployed frontend URL here if you have one
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
